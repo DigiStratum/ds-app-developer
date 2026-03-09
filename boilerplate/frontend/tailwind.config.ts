217bb146044e382c55c5fb6795f154d7aeab42f6
Lucca Alma <lucca.alma@digistratum.com>
2026-03-08 21:53:46 -0700

    Fix boilerplate tailwind.config.ts path to root node_modules

--- boilerplate/frontend/tailwind.config.ts
+++ boilerplate/frontend/tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@digistratum/**/*.{js,ts,jsx,tsx}',
+    '../node_modules/@digistratum/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
